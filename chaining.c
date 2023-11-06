71d06789a2872f1c954490077add6b82f3d7f539
bishopscode <bishopslow01@gmail.com>
2023-11-06 14:29:13 +0100

    handled more logics

---
+++ chaining.c
@@ -0,0 +1,151 @@
+#include "my_shell.h"
+
+/**
+ * is_custom_chain_delimiter - test if current char in buffer is a custom chain delimiter
+ * @info: the parameter struct
+ * @buffer: the char buffer
+ * @position: address of current position in buffer
+ *
+ * Return: 1 if custom chain delimiter, 0 otherwise
+ */
+int is_custom_chain_delimiter(info_t *info, char *buffer, size_t *position)
+{
+	size_t j = *position;
+
+	if (buffer[j] == '|' && buffer[j + 1] == '|')
+	{
+		buffer[j] = 0;
+		j++;
+		info->cmd_buf_type = CUSTOM_CMD_OR;
+	}
+	else if (buffer[j] == '&' && buffer[j + 1] == '&')
+	{
+		buffer[j] = 0;
+		j++;
+		info->cmd_buf_type = CUSTOM_CMD_AND;
+	}
+	else if (buffer[j] == ';') /* found end of this command */
+	{
+		buffer[j] = 0; /* replace semicolon with null */
+		info->cmd_buf_type = CUSTOM_CMD_CHAIN;
+	}
+	else
+		return (0);
+	*position = j;
+	return (1);
+}
+
+/**
+ * check_custom_chain - checks whether we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buffer: the char buffer
+ * @position: address of current position in buffer
+ * @start_pos: starting position in buffer
+ * @length: length of buffer
+ *
+ * Return: Void
+ */
+void check_custom_chain(info_t *info, char *buffer, size_t *position, size_t start_pos, size_t length)
+{
+	size_t j = *position;
+
+	if (info->cmd_buf_type == CUSTOM_CMD_AND)
+	{
+		if (info->status)
+		{
+			buffer[start_pos] = 0;
+			j = length;
+		}
+	}
+	if (info->cmd_buf_type == CUSTOM_CMD_OR)
+	{
+		if (!info->status)
+		{
+			buffer[start_pos] = 0;
+			j = length;
+		}
+	}
+
+	*position = j;
+}
+
+/**
+ * custom_replace_alias - replaces custom aliases in the tokenized string
+ * @info: the parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int custom_replace_alias(info_t *info)
+{
+	int i;
+	list_t *node;
+	char *alias_value;
+
+	for (i = 0; i < 10; i++)
+	{
+		node = custom_node_starts_with(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		alias_value = custom_strdup(custom_strchr(node->str, '=') + 1);
+		if (!alias_value)
+			return (0);
+		info->argv[0] = alias_value;
+	}
+	return (1);
+}
+
+/**
+ * custom_replace_vars - replaces custom variables in the tokenized string
+ * @info: the parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int custom_replace_vars(info_t *info)
+{
+	int i = 0;
+	list_t *node;
+
+	for (i = 0; info->argv[i]; i++)
+	{
+		if (info->argv[i][0] != '$' || !info->argv[i][1])
+			continue;
+
+		if (custom_strcmp(info->argv[i], "$?") == 0)
+		{
+			custom_replace_string(&(info->argv[i]),
+				custom_strdup(custom_convert_number(info->status, 10, 0)));
+			continue;
+		}
+		if (custom_strcmp(info->argv[i], "$$") == 0)
+		{
+			custom_replace_string(&(info->argv[i]),
+				custom_strdup(custom_convert_number(getpid(), 10, 0)));
+			continue;
+		}
+		node = custom_node_starts_with(info->env, &info->argv[i][1], '=');
+		if (node)
+		{
+			custom_replace_string(&(info->argv[i]),
+				custom_strdup(custom_strchr(node->str, '=') + 1));
+			continue;
+		}
+		custom_replace_string(&info->argv[i], custom_strdup(""));
+
+	}
+	return (0);
+}
+
+/**
+ * custom_replace_string - replaces a string
+ * @old_string: address of old string
+ * @new_string: new string
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int custom_replace_string(char **old_string, char *new_string)
+{
+	custom_free(*old_string);
+	*old_string = new_string;
+	return (1);
+}
--- my_shell.h
+++ my_shell.h
@@ -1,21 +1,33 @@
-#ifndef "myshell.h"
-#define "myshell.h"
+#ifndef MY_SHELL_H
+#define MY_SHELL_H
 
+/**
+ * struct custom_info_s - holds parameters and return data
+ * @alias: the alias list
+ * @argv: argument vector
+ * @buf: buffer for getline
+ * @cmd_buf_type: the type of command buffer
+ * @env: the environment
+ * @err_num: the error number
+ * @history: command history
+ * @line_count: the command line count
+ * @linecount_flag: a flag for counting lines
+ * @path: the command path
+ * @status: the exit status
+ */
+typedef struct custom_info_s
+{
+	list_t *alias;
+	char **argv;
+	char *buf;
+	int cmd_buf_type;
+	list_t *env;
+	int err_num;
+	list_t *history;
+	int line_count;
+	int linecount_flag;
+	char *path;
+	int status;
+} custom_info_t;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
+#endif /* My_SHELL_H */
